13d3cc0808e9235c37b54962f0a3e1fb131d71c5
ahmet aktas <ahmetmunip38@gmail.com>
2023-03-06 22:32:44 +0100

    ekleme yapildi

--- src/Menu.java
+++ src/Menu.java
@@ -7,6 +7,7 @@ public class Menu {
         //Mc Donald's ekran ciktisi ve Menu gosterimi
         //hangi menuyu sectigi sorulur
         //icecek tercihi sorulur
+
         //icecek boyutu sorulur
         //chips tercihi sorulur
         // chips boyutu sorulur
@@ -15,10 +16,57 @@ public class Menu {
 
            menusecimi();
            String menusecim=menusecimi();
+           icecekTercihi();
 
 
 
 
+    }
+
+    private static void icecekTercihi() {
+        System.out.println("Hangi icecegi tercih ediyorsunuz?");
+        System.out.println("\n1. Cocacola"+"\n2. Fanta"+"\n3. Sprite"+"\n4. Nescafe"+"\n5. Cay");
+        int tercihi=sc.nextInt();
+        switch (tercihi) {
+            case 1:
+                System.out.println("Cocacola tercih ettiniz..");
+                boyutSecimi();
+                break;
+            case 2:
+                System.out.println("Fanta tercih ettiniz..");
+                boyutSecimi();
+                break;
+            case 3:
+                System.out.println("Sprite tercih ettiniz..");
+                boyutSecimi();
+                break;
+            case 4:
+                System.out.println("Nescafe tercih ettiniz..");
+                boyutSecimi();
+                break;
+            case 5:
+                System.out.println("Cay tercih ettiniz..");
+                break;
+            default:
+                System.out.println("Yanlis bir tercih yaptiniz.Lutfen sayi giriniz..");
+                icecekTercihi();
+        }
+    }
+
+    private static void boyutSecimi() {
+        System.out.println("Lutfen boyut secimi yapiniz  :");
+        System.out.println("\nBuyuk \nOrta  \nKucuk ");
+        String secimBoyut=sc.next();
+        if(secimBoyut.equals("buyuk")){
+            System.out.println("Iceceginiz "+ secimBoyut +" boy olarak hazirlaniyor");
+        }else if(secimBoyut.equals("orta")){
+            System.out.println("Iceceginiz "+ secimBoyut +" boy olarak hazirlaniyor");
+        }else if(secimBoyut.equals("kucuk")){
+            System.out.println("Iceceginiz "+ secimBoyut +" boy olarak hazirlaniyor");
+        }else{
+            System.out.println("Yanlis bir tercih yaptiniz.Lutfen tekrar giriniz..");
+            boyutSecimi();
+        }
 
     }
 
